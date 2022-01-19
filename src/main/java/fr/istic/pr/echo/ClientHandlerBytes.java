a8d71fef770d12cfd7ca045267641fcdbefa00c6
Ammar Kazem <kazem.ammar49@gmail.com>
2022-01-19 08:24:28 +0100

    First exercice done

--- src/main/java/fr/istic/pr/echo/ClientHandlerBytes.java
+++ src/main/java/fr/istic/pr/echo/ClientHandlerBytes.java
@@ -1,18 +1,35 @@
 
 package fr.istic.pr.echo;
 
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class ClientHandlerBytes implements ClientHandler {
 
     private Socket socket;
+    //Lecture du message dans un buffer de bytes
+    private byte[] buffer;
 
     public ClientHandlerBytes(Socket socket){
         this.socket = socket;
     }
 
-    public void handle() {
-        //Lecture du message dans un buffer de bytes
-        //Envoie du buffer de bytes.
+    public void handle() throws IOException {
+        // Initialisation du buffer
+        buffer = new byte[8];
+
+        // Récupération du message envoyer par le client
+        InputStream input = socket.getInputStream();
+        input.read(buffer);
+        String inputToString = new String(buffer);
+        System.out.println("Message sent by the client: " + inputToString);
+
+        OutputStream output = socket.getOutputStream();
+        output.write(buffer);
+        // Message en bytes ==> string
+        /*String inputToString = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
+        System.out.println(inputToString);*/
     }
 }
--- src/main/java/fr/istic/pr/echo/EchoServer.java
+++ src/main/java/fr/istic/pr/echo/EchoServer.java
@@ -1,8 +1,36 @@
 package fr.istic.pr.echo;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        // Création de la socket serveur sur le port 8080
+        int listeningPort = 8080;
+        ServerSocket socketServer = new ServerSocket(listeningPort);
+
+        try {
+            // Attente de connexion d'un client (pas de timeout ici)
+            System.out.println("Waiting for clients");
+            Socket socketClient = socketServer.accept();
+            System.out.println("The client " + socketClient.getInetAddress() + " is connected");
+
+            // Création d'un ClientHandler
+            ClientHandler clientHandler = new ClientHandlerBytes(socketClient);
+
+            while (true) {
+                // Appel de la fonction handle()
+                clientHandler.handle();
+            }
+        } catch (IOException exception){
+            System.out.println("Error " + exception.getMessage());
+        } finally {
+            socketServer.close();
+        }
         //Attente sur le port 8080
 
         /* Pour chaque client : 
